029137463fe706be79c00c896eaba3d1113f2cee
Eduardo Malvino <emalvino@hexacta.com>
2015-08-28 16:53:47 -0300

    cosas en contexto

--- soundword/app/src/main/assets/www/js/wordGameContextoCosas.js
+++ soundword/app/src/main/assets/www/js/wordGameContextoCosas.js
@@ -2,84 +2,413 @@ var niveles_palabras_contexto_cosas = [ //Niveles
   [ //Nivel
     [ //Sub-nivel
       {
-        'text': 'Perro',
-        'sound': '/android_asset/www/audio/perro.mp3', 
-        'wordSound' : '/android_asset/www/audio/streak_1-4.mp3'
+        'text': 'Escalar',
+        'sound': '/android_asset/www/audio/contexto_escalar.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_escalar.mp3'
       }, 
       {
-        'image': 'images/dog2.jpg', 
+        'image': 'images/escalar.jpg', 
         'points':100, 
         'type': CORRECTA
       }, 
       {
-        'image': 'images/gato.jpg', 
+        'image': 'images/nadar.jpg', 
         'points':50, 
         'type': DISTRACTOR_SEMANTICO
-      },
-      {
-        'image': 'images/rabbit.jpg', 
-        'points':20, 
-        'type': DISTRACTOR_SEMANTICO
-      }, 
-      {
-        'image': 'images/arbol.jpg', 
-        'type': DISTRACTOR_NO_RELACIONADO
       }
     ],
     [ //Sub-nivel
       {
-        'text': 'Copa',
-        'sound': '/android_asset/www/audio/streak_1-1.mp3', 
-        'wordSound' : '/android_asset/www/audio/streak_1-4.mp3'
+        'text': 'Nadar',
+        'sound': '/android_asset/www/audio/contexto_nadar.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_nadar.mp3'
       }, 
       {
-        'image': 'images/italia90header.jpg', 
-        'points':0, 
-        'type': DISTRACTOR_FORMAL
-      }, 
-      {
-        'image': 'images/youwin.jpg', 
+        'image': 'images/nadar.jpg', 
         'points':100, 
         'type': CORRECTA
+      }, 
+      {
+        'image': 'images/escalar.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
       }
     ],
-  ],
-  [ //Nivel
-    [//Sub-nivel
+    [ //Sub-nivel
       {
-        'text': 'Plaza',
-        'sound': '/android_asset/www/audio/streak_1-1.mp3', 
-        'wordSound' : '/android_asset/www/audio/streak_1-4.mp3'
+        'text': 'Bostezar',
+        'sound': '/android_asset/www/audio/contexto_bostezar.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_bostezar.mp3'
       }, 
       {
-        'image': 'images/plaza.jpg', 
+        'image': 'images/bostezar.jpg', 
         'points':100, 
         'type': CORRECTA
       }, 
       {
-        'image': 'images/correct.jpg', 
-        'points':10, 
-        'type': DISTRACTOR_NO_RELACIONADO
+        'image': 'images/estornudar.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
       }
-    ]
-  ],
-  [ //Nivel
-    [//Sub-nivel
-     {
-      'text': 'Plaza' , 
-      'sound': '/android_asset/www/audio/streak_1-1.mp3', 
-      'wordSound' : '/android_asset/www/audio/streak_1-4.mp3'
-    }, 
-    {
-      'image': 'images/plaza.jpg', 
-      'points':100, 
-      'type': CORRECTA
-    }, 
-    {
-      'image': 'images/perro.jpg', 
-      'points':10, 
-      'type': DISTRACTOR_NO_RELACIONADO
-    }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Estornudar',
+        'sound': '/android_asset/www/audio/contexto_estornudar.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_estornudar.mp3'
+      }, 
+      {
+        'image': 'images/estornudar.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/bostezar.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Revolver',
+        'sound': '/android_asset/www/audio/contexto_revolver2.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_revolver2.mp3'
+      }, 
+      {
+        'image': 'images/revolver2.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/batir.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Batir',
+        'sound': '/android_asset/www/audio/contexto_batir.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_batir.mp3'
+      }, 
+      {
+        'image': 'images/batir.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/revolver2.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Lavar',
+        'sound': '/android_asset/www/audio/contexto_lavar.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_lavar.mp3'
+      }, 
+      {
+        'image': 'images/lavar.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/barrer.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Barrer',
+        'sound': '/android_asset/www/audio/contexto_barrer.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_barrer.mp3'
+      }, 
+      {
+        'image': 'images/barrer.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/lavar.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Hervír',
+        'sound': '/android_asset/www/audio/contexto_hervir.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_hervir.mp3'
+      }, 
+      {
+        'image': 'images/hervir.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/freir.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Freír',
+        'sound': '/android_asset/www/audio/contexto_freir.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_freir.mp3'
+      }, 
+      {
+        'image': 'images/freir.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/hervir.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Llover',
+        'sound': '/android_asset/www/audio/contexto_llover.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_llover.mp3'
+      }, 
+      {
+        'image': 'images/llover.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/nevar.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Nevar',
+        'sound': '/android_asset/www/audio/contexto_nevar.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_nevar.mp3'
+      }, 
+      {
+        'image': 'images/nevar.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/llover.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Ver',
+        'sound': '/android_asset/www/audio/contexto_ver.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_ver.mp3'
+      }, 
+      {
+        'image': 'images/ver.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/tocar.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Tocar',
+        'sound': '/android_asset/www/audio/contexto_tocar.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_tocar.mp3'
+      }, 
+      {
+        'image': 'images/tocar.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/ver.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Oler',
+        'sound': '/android_asset/www/audio/contexto_oler.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_oler.mp3'
+      }, 
+      {
+        'image': 'images/oler.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/oir.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Oír',
+        'sound': '/android_asset/www/audio/contexto_oir.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_oir.mp3'
+      }, 
+      {
+        'image': 'images/oir.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/oler.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Volar',
+        'sound': '/android_asset/www/audio/contexto_volar.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_volar.mp3'
+      }, 
+      {
+        'image': 'images/volar.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/navegar.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Navegar',
+        'sound': '/android_asset/www/audio/contexto_navegar.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_navegar.mp3'
+      }, 
+      {
+        'image': 'images/navegar.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/volar.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Cortar',
+        'sound': '/android_asset/www/audio/contexto_cortar.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_cortar.mp3'
+      }, 
+      {
+        'image': 'images/cortar.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/pinchar.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Pinchar',
+        'sound': '/android_asset/www/audio/contexto_pinchar.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_pinchar.mp3'
+      }, 
+      {
+        'image': 'images/pinchar.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/cortar.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Comprar',
+        'sound': '/android_asset/www/audio/contexto_comprar.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_comprar.mp3'
+      }, 
+      {
+        'image': 'images/comprar.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/vender.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Vender',
+        'sound': '/android_asset/www/audio/contexto_vender.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_vender.mp3'
+      }, 
+      {
+        'image': 'images/vender.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/comprar.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Subir',
+        'sound': '/android_asset/www/audio/contexto_subir.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_subir.mp3'
+      }, 
+      {
+        'image': 'images/subir.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/bajar.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
+    ],
+    [ //Sub-nivel
+      {
+        'text': 'Bajar',
+        'sound': '/android_asset/www/audio/contexto_bajar.mp3', 
+        'wordSound' : '/android_asset/www/audio/word_bajar.mp3'
+      }, 
+      {
+        'image': 'images/bajar.jpg', 
+        'points':100, 
+        'type': CORRECTA
+      }, 
+      {
+        'image': 'images/subir.jpg', 
+        'points':50, 
+        'type': DISTRACTOR_SEMANTICO
+      }
     ]
   ]              
 ];
+
+
